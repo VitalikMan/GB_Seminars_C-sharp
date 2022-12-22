64f91bcbb82feaf7468d60887a3bc6c960e6cc83
Vitaly <m.v.a.-1992@mail.ru>
2022-12-22 19:40:43 +1000

    add Task1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+Clear();
+
+int number = new Random().Next(10, 100);
+
+WriteLine(number);
+
+//version 1:
+
+WriteLine(number / 10 > number % 10 ? number / 10 : number % 10);
+
+//version 2:
+
+// if (number / 10 > number % 10)
+// {
+//     WriteLine(number / 10);
+// }
+// else
+// {
+//     WriteLine(number % 10);
+// }
