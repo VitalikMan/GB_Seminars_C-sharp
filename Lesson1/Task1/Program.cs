9f65dcc59d872bb687e4712270b523629e9c3e0a
Vitaly <m.v.a.-1992@mail.ru>
2022-12-22 19:28:13 +1000

    add Task1 Task2 Task3 Task4

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Write("Введите первое число: ");
+int a = Convert.ToInt32(ReadLine()!);
+
+Write("Введите второе число: ");
+int b = Convert.ToInt32(ReadLine()!);
+
+if (a == b * b)
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+
+Write("Введите число: ");
+int num = Convert.ToInt32(ReadLine()!);
+
+int last_num = num % 10;
+
+Write($"Последняя цифра числа {num}: {last_num}");
