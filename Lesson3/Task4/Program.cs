dfa5f4a58a7511e72f6214a118d84288bb6a3f20
Vitaly <m.v.a.-1992@mail.ru>
2022-12-24 21:15:08 +1000

    add Task 4

---
+++ Lesson3/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача:
+// Токарей = 3,
+// 2 токаря - 1 разряда. Каждый из них делает по 1 детали
+// 1 токарь 4 разряда. Токарь 4 разряда делат в два раза больше чем 2 токаря вместе.
+// Вычислите, возможно ли такое?
+
+using static System.Console;
+
+Clear();
+
+Write("Введите количество деталей: ");
+int i = int.Parse(ReadLine()!);
+
+int tokar_1r = 1;
+int tokar_4r = 2 * (tokar_1r + tokar_1r);
+
+int sum_proizvodstvo = tokar_1r + tokar_1r + tokar_4r;
+
+if (i % sum_proizvodstvo == 0)
+{
+    WriteLine("Это правда");
+    WriteLine($"Первое двое токарей 1-го разряда, сделали по {i / 6}");
+    WriteLine($"Третий токарь 4-го разряда, сделал {i / 6 + 4}");
+}
+else
+{
+    WriteLine("Они лгут!");
+}
