84fc51528049b5bab9aec9d9a7d73dcdd8be72bb
Vitaly <m.v.a.-1992@mail.ru>
2022-12-24 21:15:17 +1000

    add Task 5

---
+++ Lesson3/Task5/Program.cs
@@ -0,0 +1,57 @@
+﻿using static System.Console;
+
+Clear();
+
+Write("Введите номер дня недели: ");
+
+bool isParse = int.TryParse(ReadLine(), out int i);
+
+if (!isParse)
+{
+    WriteLine("Введено не число!");
+    return;
+}
+
+switch (i)
+{
+    case 1:
+        {
+            WriteLine("Понедельник");
+            break;
+        }
+    case 2:
+        {
+            WriteLine("Вторник");
+            break;
+        }
+    case 3:
+        {
+            WriteLine("Среда");
+            break;
+        }
+    case 4:
+        {
+            WriteLine("Четверг");
+            break;
+        }
+    case 5:
+        {
+            WriteLine("Пятница");
+            break;
+        }
+    case 6:
+        {
+            WriteLine("Субботу");
+            break;
+        }
+    case 7:
+        {
+            WriteLine("Воскресенье");
+            break;
+        }
+    default:
+        {
+            WriteLine("Нет такого дня недели");
+            break;
+        }
+}
