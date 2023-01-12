66c2894867096a1ce4266c02d4ec9fab1e1de29b
Vitaly <m.v.a.-1992@mail.ru>
2023-01-13 05:18:57 +1000

    add Task 2

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+
+// 78 -> 2
+
+// 89126 -> 5
+
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+int num = int.Parse(ReadLine()!);
+
+int SumNum = GetSumNum(num);
+WriteLine($"Количество цифр в числе в {num} = {SumNum}");
+
+int GetSumNum(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result++;
+        num /= 10;
+    }
+
+    return result;
+}
