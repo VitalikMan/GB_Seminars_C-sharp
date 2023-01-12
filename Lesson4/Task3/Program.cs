7954740f180468f7a866e412311380a0e453563b
Vitaly <m.v.a.-1992@mail.ru>
2023-01-13 05:19:08 +1000

    add Task 3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+// 4  -> 24
+// 5  -> 120
+
+// Доп.условие: при условии, что N будет делиться на это число без остатка.
+
+
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+int num = int.Parse(ReadLine()!);
+
+int SumNum = GetSumNum(num);
+WriteLine($"Произведение ряда чисел от 1 до {num} = {SumNum}");
+
+int GetSumNum(int num)
+{
+    int result = 1;
+    while (num > 0)
+    {
+        result *= num;
+        num--;
+    }
+
+    return result;
+}
