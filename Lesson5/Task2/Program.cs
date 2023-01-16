a4c549cfa5091c5ebf7efb719ae4d789345a02fb
Vitaly <m.v.a.-1992@mail.ru>
2023-01-17 05:30:39 +1000

    add Task 2

---
+++ Lesson5/Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Напишите программу замена элементов массива: положительные элементы замените на
+соответствующие отрицательные, и наоборот.
+*/
+
+using static System.Console;
+Clear();
+
+int[] array = GetArray(10, -10, 10);
+WriteLine("Массив до изменений");
+printArray(array);
+ChangeArray(array);
+WriteLine("Массив после изменений");
+printArray(array);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+
+    int[] resultArray = new int[size];
+
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = random.Next(minValue, maxValue + 1);
+    }
+
+    return resultArray;
+}
+
+void printArray(int[] array)
+{
+    Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Write($"{array[i]}, ");
+    }
+
+    WriteLine($"{array[array.Length - 1]}]");
+}
+
+void ChangeArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
