3a7bef08f911eb23570ff958b4300cb808f38293
Vitaly <m.v.a.-1992@mail.ru>
2023-01-17 05:30:49 +1000

    add Task 3

---
+++ Lesson5/Task3/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+3; массив [6, 7, 19, 345, 3] -> да
+*/
+
+using static System.Console;
+Clear();
+
+Write("Введи число: ");
+int number = int.Parse(ReadLine()!);
+
+int[] array = GetArray(10, 0, 5);
+
+bool isInArray = check_number_in_array(number, array);
+printArray(array, number, isInArray);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+
+    int[] resultArray = new int[size];
+
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = random.Next(minValue, maxValue + 1);
+    }
+
+    return resultArray;
+}
+
+bool check_number_in_array(int number, int[] array)
+{
+    bool result = false;
+    foreach (int i in array)
+    {
+        if (number == i) result = true;
+    }
+
+    return result;
+}
+
+void printArray(int[] array, int number, bool flag)
+{
+    if (flag) Write($"Число {number} присутствует в массиве: ");
+    else Write($"Число {number} отсутствует в массиве: ");
+    Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Write($"{array[i]}, ");
+    }
+
+    WriteLine($"{array[array.Length - 1]}]");
+}
