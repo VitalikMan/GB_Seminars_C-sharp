d75ce2a6224a84fe2e7366ddefc0f0beefaa955f
Vitaly <m.v.a.-1992@mail.ru>
2023-01-20 05:29:42 +1000

    add Task 3

---
+++ Lesson6/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int num = int.Parse(ReadLine()!);
+WriteLine($"Число в двоичной системе счисления: {ConvertTo2(num)}");
+
+string ConvertTo2(int number)
+{
+    if (number == 0) return "0";
+    string result = string.Empty;
+    while (number > 0)
+    {
+        result = number % 2 + result;
+        number /= 2;
+    }
+    return result;
+}
