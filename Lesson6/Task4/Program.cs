7ca96c53f324031078a684494f1dd454cfb1f6ee
Vitaly <m.v.a.-1992@mail.ru>
2023-01-20 05:29:51 +1000

    add Task 4

---
+++ Lesson6/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 5 -> 0 1 1 2 3
+
+// Если N = 3 -> 0 1 1
+
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+fibonacci(number);
+
+void fibonacci(int number)
+{
+    int temp, num1 = 0, num2 = 1;
+    Write($"{num1} {num2} ");
+    for (int i = 2; i < number; i++)
+    {
+        temp = num1 + num2;
+        num1 = num2;
+        num2 = temp;
+        Write($"{num2} ");
+    }
+}
