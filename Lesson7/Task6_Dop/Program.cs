8d1491cdbcd2d0a5b181ae5f0fa5fdd3846116d3
Vitaly <m.v.a.-1992@mail.ru>
2023-01-27 01:16:54 +1000

    add Task 6_Dop

---
+++ Lesson7/Task6_Dop/Program.cs
@@ -0,0 +1,6 @@
+﻿// Команды для создания библиотек:
+// dotnet new sln - o Tasks
+// dotnet new classlib
+// dotnet sln Tasks.sln add.\MyLib\MyLib.csproj
+// dotnet sln Tasks.sln add .\Task0\Task0.csproj
+// dotnet add.\Task0\Task0.csproj reference .\MyLib\MyLib.csproj
