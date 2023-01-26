4d400a4d27e037168e814abc7e4b8ff0394ab6ae
Vitaly <m.v.a.-1992@mail.ru>
2023-01-27 01:11:52 +1000

    add Task 1

---
+++ Lesson7/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+// m = 3, n = 4.
+
+// 1 4 8 19
+
+// 5 -2 33 -2
+
+// 77 3 8 1
+
+using static System.Console;
+
+Clear();
+
+Write("Введите размер матрицы и диапазон значений через пробел: ");
+// string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] intParams = Array.ConvertAll(ReadLine()!.Split(new char[] { '\"', '#', ';', ',', 's' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+// int[,] array = GetMatrixArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]), int.Parse(parameters[3]));
+int[,] array = GetMatrixArray(intParams[0], intParams[1], intParams[2], intParams[3]);
+
+PrintMatrixArray(array);
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] resultArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],5} ");
+        }
+        WriteLine();
+    }
+}
